e458d66147ffa077880858937470e7e0eccf04c6
Simon Taveirne <84476802+Sim-Teammario@users.noreply.github.com>
2026-03-25 19:48:41 +0100

    Add initial PHP application structure with Twig

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Charger les variables d'environnement
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->safeLoad();
+
+// Initialiser Twig
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../app/Views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => $_ENV['APP_ENV'] === 'production' ? __DIR__ . '/../templates' : false,
+]);
+
+$twig->addGlobal('session', $_SESSION);
+$twig->addGlobal('cookie_consent', $_COOKIE['rgpd_consent'] ?? false);
+
+// Routeur simple
+$url = $_GET['url'] ?? '/';
+$routes = require __DIR__ . '/../config/routes.php';
+
+if (array_key_exists($url, $routes)) {
+    [$controllerName, $method] = $routes[$url];
+    $controller = new $controllerName($twig);
+    $controller->$method();
+} else {
+    http_response_code(404);
+    echo $twig->render('page.twig', ['title' => '404', 'content' => 'Page introuvable.']);
+}
