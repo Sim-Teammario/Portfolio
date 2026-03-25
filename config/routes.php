07862aa374e28f3d224c045dc8ebb2b6df3953f7
Simon Taveirne <84476802+Sim-Teammario@users.noreply.github.com>
2026-03-25 19:50:01 +0100

    Add routes.php
    Add routing configuration for application

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    '/' => ['App\Controllers\HomeController', 'index'],
+    'diplomes' => ['App\Controllers\PageController', 'diplomas'],
+    'projets' => ['App\Controllers\PageController', 'projects'],
+    'a-propos' => ['App\Controllers\PageController', 'about'],
+    'actuellement' => ['App\Controllers\PageController', 'currently'],
+    'mentions-legales' => ['App\Controllers\PageController', 'mentions'],
+    'politique-confidentialite' => ['App\Controllers\PageController', 'privacy'],
+    'contact' => ['App\Controllers\ContactController', 'index'],
+    'contact/submit' => ['App\Controllers\ContactController', 'submit'],
+    'cv/download' => ['App\Controllers\CvController', 'download'],
+];
